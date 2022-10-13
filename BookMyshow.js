1dcb7276e851517233994a7cafdde8a3155eab9f
Bhargavi Panga <pangabhargavi98@gmail.com>
2022-10-14 00:16:18 +0530

    FirstCommit

---
+++ BookMyshow.js
@@ -0,0 +1,687 @@
+let movies = [
+  {
+    id: 1,
+    userId: 1001,
+    TypeofSeat: 'Premium',
+    seatId: "1A"
+  },
+   {
+    id: 2,
+    userId: 1002,
+    TypeofSeat: 'Premium',
+    seatId: "2A"
+  },
+  {
+    id: 3,
+    userId: 1003,
+    TypeofSeat: 'Premium',
+    seatId: "3A"
+  }
+  , {
+    id: 4,
+    userId: 1004,
+    TypeofSeat: 'Premium',
+    seatId: "4A"
+  },
+  {
+    id: 5,
+    userId: 1005,
+    TypeofSeat: 'Premium',
+    seatId: "5A"
+  },
+  {
+    id: 6,
+    userId: 1006,
+    TypeofSeat: 'Premium',
+    seatId: "6A"
+  },
+  {
+    id: 7,
+    userId: 1007,
+    TypeofSeat: 'Premium',
+    seatId: "7A"
+  },
+  {
+    id: 8,
+    userId: 1008,
+    TypeofSeat: 'Premium',
+    seatId: "8A"
+  },
+  {
+    id: 9,
+    userId: 1009,
+    TypeofSeat: 'Premium',
+    seatId: "9A"
+  },
+  {
+    id: 10,
+    userId: 1010,
+    TypeofSeat: 'Premium',
+    seatId: "10A"
+  },
+  {
+    id: 11,
+    userId: 1011,
+    TypeofSeat: 'Premium',
+    seatId: "1B"
+  },
+  {
+    id: 12,
+    userId: 1012,
+    TypeofSeat: 'Premium',
+    seatId: "2B"
+  },
+  {
+    id: 13,
+    userId: 1013,
+    TypeofSeat: 'Premium',
+    seatId: "3B"
+  },
+  {
+    id: 14,
+    userId: 1014,
+    TypeofSeat: 'Premium',
+    seatId: "4B"
+  },
+  {
+    id: 15,
+    userId: 1015,
+    TypeofSeat: 'Premium',
+    seatId: "5B"
+  },
+  {
+    id: 16,
+    userId: 1016,
+    TypeofSeat: 'Premium',
+    seatId: "6B"
+  },
+  {
+    id: 17,
+    userId: 1017,
+    TypeofSeat: 'Premium',
+    seatId: "7B"
+  },
+  {
+    id: 18,
+    userId: 1018,
+    TypeofSeat: 'Premium',
+    seatId: "8B"
+  },
+  {
+    id: 19,
+    userId: 1019,
+    TypeofSeat: 'Premium',
+    seatId: "9B"
+  },
+  {
+    id: 20,
+    userId: 1020,
+    TypeofSeat: 'Premium',
+    seatId: "10B"
+  },
+  {
+    id: 21,
+    userId: 1021,
+    TypeofSeat: 'Premium',
+    seatId: "1C"
+  },
+  {
+    id: 22,
+    userId: 1022,
+    TypeofSeat: 'Premium',
+    seatId: "2C"
+  },
+  {
+    id: 23,
+    userId: 1023,
+    TypeofSeat: 'Premium',
+    seatId: "3C"
+  },
+  {
+    id: 24,
+    userId: 1024,
+    TypeofSeat: 'Premium',
+    seatId: "4C"
+  },
+  {
+    id: 25,
+    userId: 1025,
+    TypeofSeat: 'Premium',
+    seatId: "5C"
+  },
+  {
+    id: 26,
+    userId: 1026,
+    TypeofSeat: 'Premium',
+    seatId: "6C"
+  },
+  {
+    id: 27,
+    userId: 1027,
+    TypeofSeat: 'Premium',
+    seatId: "7C"
+  },
+  {
+    id: 28,
+    userId: 1028,
+    TypeofSeat: 'Premium',
+    seatId: "8C"
+  },
+  {
+    id: 29,
+    userId: 1029,
+    TypeofSeat: 'Premium',
+    seatId: "9C"
+  },
+  {
+    id: 30,
+    userId: 1030,
+    TypeofSeat: 'Premium',
+    seatId: "10C"
+  },
+  {
+    id: 31,
+    userId: 1031,
+    TypeofSeat: 'Premium',
+    seatId: "1D"
+  },
+  {
+    id: 32,
+    userId: 1032,
+    TypeofSeat: 'Premium',
+    seatId: "2D"
+  },
+  {
+    id: 33,
+    userId: 1033,
+    TypeofSeat: 'Premium',
+    seatId: "3D"
+  },
+  {
+    id: 34,
+    userId: 1034,
+    TypeofSeat: 'Premium',
+    seatId: "4D"
+  },
+  {
+    id: 35,
+    userId: 1035,
+    TypeofSeat: 'Premium',
+    seatId: "5D"
+  },
+  {
+    id: 36,
+    userId: 1036,
+    TypeofSeat: 'Premium',
+    seatId: "6D"
+  },
+  {
+    id: 37,
+    userId: 1037,
+    TypeofSeat: 'Premium',
+    seatId: "7D"
+  },
+  {
+    id: 38,
+    userId: 1038,
+    TypeofSeat: 'Premium',
+    seatId: "8D"
+  },
+  {
+    id: 39,
+    userId: 1039,
+    TypeofSeat: 'Premium',
+    seatId: "9D"
+  },
+  {
+    id: 40,
+    userId: 1040,
+    TypeofSeat: 'Premium',
+    seatId: "10D"
+  },
+  {
+    id: 41,
+    userId: 1041,
+    TypeofSeat: 'standard',
+    seatId: "1E"
+  },
+  {
+    id: 42,
+    userId: 1042,
+    TypeofSeat: 'standard',
+    seatId: "2E"
+  },
+  {
+    id: 43,
+    userId: 1043,
+    TypeofSeat: 'standard',
+    seatId: "3E"
+  },
+  {
+    id: 44,
+    userId: 1044,
+    TypeofSeat: 'standard',
+    seatId: "4E"
+  },
+  {
+    id: 45,
+    userId: 1045,
+    TypeofSeat: 'standard',
+    seatId: "5E"
+  },
+  {
+    id: 46,
+    userId: 1046,
+    TypeofSeat: 'standard',
+    seatId: "6E"
+  },
+  {
+    id: 47,
+    userId: 1047,
+    TypeofSeat: 'standard',
+    seatId: "7E"
+  },
+  {
+    id: 48,
+    userId: 1048,
+    TypeofSeat: 'standEard',
+    seatId: "8E"
+  },
+  {
+    id: 49,
+    userId: 1049,
+    TypeofSeat: 'standard',
+    seatId: "9E"
+  },
+  {
+    id: 50,
+    userId: 1050,
+    TypeofSeat: 'standard',
+    seatId: "10E"
+  },
+  {id: 51,
+    userId: 1051,
+    TypeofSeat: 'standard',
+    seatId: "1F"
+  },
+  {
+    id: 52,
+    userId: 1052,
+    TypeofSeat: 'standard',
+    seatId: "2F"
+  },
+  {
+    id: 53,
+    userId: 1053,
+    TypeofSeat: 'standard',
+    seatId: "3F"
+  },
+  {
+    id: 54,
+    userId: 1054,
+    TypeofSeat: 'standard',
+    seatId: "4F"
+  },
+  {
+    id: 55,
+    userId: 1055,
+    TypeofSeat: 'standard',
+    seatId: "5F"
+  },
+  {
+    id: 56,
+    userId: 1056,
+    TypeofSeat: 'standard',
+    seatId: "6F"
+  },
+  {
+    id: 57,
+    userId: 1057,
+    TypeofSeat: 'standard',
+    seatId: "7F"
+  },
+  {
+    id: 58,
+    userId: 1058,
+    TypeofSeat: 'standard',
+    seatId: "8F"
+  },
+  {
+    id: 59,
+    userId: 1059,
+    TypeofSeat: 'standard',
+    seatId: "9F"
+  },
+  {
+    id: 60,
+    userId: 1060,
+    TypeofSeat: 'standard',
+    seatId: "10F"
+  },
+  {
+    id: 61,
+    userId: 1061,
+    TypeofSeat: 'standard',
+    seatId: "1G"
+  },
+  {
+    id: 62,
+    userId: 1062,
+    TypeofSeat: 'standard',
+    seatId: "2G"
+  },
+  {
+    id: 63,
+    userId: 1063,
+    TypeofSeat: 'standard',
+    seatId: "3G"
+  },
+  {
+    id: 64,
+    userId: 1064,
+    TypeofSeat: 'standard',
+    seatId: "4G"
+  },
+  {
+    id: 65,
+    userId: 1065,
+    TypeofSeat: 'standard',
+    seatId: "5G"
+  },
+  {
+    id: 66,
+    userId: 1066,
+    TypeofSeat: 'standard',
+    seatId: "6G"
+  },
+  {
+    id: 67,
+    userId: 1067,
+    TypeofSeat: 'standard',
+    seatId: "7G"
+  },
+  {
+    id: 68,
+    userId: 1068,
+    TypeofSeat: 'standard',
+    seatId: "8G"
+  },
+  {
+    id: 69,
+    userId: 1069,
+    TypeofSeat: 'standard',
+    seatId: "9G"
+  },
+  {
+    id: 70,
+    userId: 1070,
+    TypeofSeat: 'standard',
+    seatId: "10G"
+  },
+  {
+    id: 71,
+    userId: 1071,
+    TypeofSeat: 'standard',
+    seatId: "1H"
+  },
+  {
+    id: 72,
+    userId: 1072,
+    TypeofSeat: 'standard',
+    seatId: "2H"
+  },
+  {
+    id: 73,
+    userId: 1073,
+    TypeofSeat: 'standard',
+    seatId: "3H"
+  },
+  {
+    id: 74,
+    userId: 1074,
+    TypeofSeat: 'standard',
+    seatId: "4H"
+  },
+  {
+    id: 75,
+    userId: 1075,
+    TypeofSeat: 'standard',
+    seatId: "5H"
+  },
+  {
+    id: 76,
+    userId: 1076,
+    TypeofSeat: 'standard',
+    seatId: "6H"
+  },
+  {
+    id: 77, userId: 1077,
+    TypeofSeat: 'standard',
+    seatId: "7H"
+  },
+  {
+    id: 78,
+    userId: 1078,
+    TypeofSeat: 'standard',
+    seatId: "8H"
+  },
+  {
+    id: 79,
+    userId: 1079,
+    TypeofSeat: 'standard',
+    seatId: "9H"
+  },
+  {
+    id: 80,
+    userId: 1080,
+    TypeofSeat: 'standard',
+    seatId: "10H"
+  },
+  {
+    id: 81,
+    userId: 1081,
+    TypeofSeat: 'standard',
+    seatId: "1I"
+  },
+  {
+    id: 82,
+    userId: 1082,
+    TypeofSeat: 'standard',
+    seatId: "2I"
+  },
+  {
+    id: 83,
+    userId: 1083,
+    TypeofSeat: 'standard',
+    seatId: "3I"
+  },
+  {
+    id: 84,
+    userId: 1084,
+    TypeofSeat: 'standard',
+    seatId: "4I"
+  },
+  {
+    id: 85,
+    userId: 1085,
+    TypeofSeat: 'standard',
+    seatId: "5I"
+  },
+  {
+    id: 86,
+    userId: 1086,
+    TypeofSeat: 'standard',
+    seatId: "6I"
+  },
+  {
+    id: 87,
+    userId: 1087,
+    TypeofSeat: 'standard',
+    seatId: "7I"
+  },
+  {
+    id: 88,
+    userId: 1088,
+    TypeofSeat: 'standard',
+    seatId: "8I"
+  },
+  {
+    id: 89,
+    userId: 1089,
+    TypeofSeat: 'standard',
+    seatId: "9I"
+  },
+  {
+    id: 90,
+    userId: 1090,
+    TypeofSeat: 'standard',
+    seatId: "10I"
+  },
+  {
+    id: 91,
+    userId: 1091,
+    TypeofSeat: 'standard',
+    seatId: "1J"
+  },
+  {
+    id: 92,
+    userId: 1092,
+    TypeofSeat: 'standard',
+    seatId: "2J"
+  },
+  {
+    id: 93,
+    userId: 1093,
+    TypeofSeat: 'standard',
+    seatId: "3J"
+  },
+  {
+    id: 94,
+    userId: 1094,
+    TypeofSeat: 'standard',
+    seatId: "4J"
+  },
+  {
+    id: 95,
+    userId: 1095,
+    TypeofSeat: 'standard',
+    seatId: "5J"
+  },
+  {
+    id: 96,
+    userId: 1096,
+    TypeofSeat: 'standard',
+    seatId: "6J"
+  },
+  {
+    id: 97,
+    userId: 1097,
+    TypeofSeat: 'standard',
+    seatId: "7J"
+  },
+  {
+    id: 98,
+    userId: 1098,
+    TypeofSeat: 'standard',
+    seatId: "8J"
+  },
+  {
+    id: 99,
+    userId: 1099,
+    TypeofSeat: 'standard',
+    seatId: "9J"
+  },
+  {
+    id: 100,
+    userId: 1100,
+    TypeofSeat: 'standard',
+    seatId: "10J"
+  }
+]
+let ticketCast = null;
+let noTickect = 0;
+document.getElementById('TicketType').addEventListener("change" , (e) => {
+    ticketCast = e.target.value;
+})
+document.getElementById('qty').addEventListener("change" , (e) => {
+  noTickect = parseInt(e.target.value);
+})
+document.getElementById("procced").addEventListener('click' ,() => {
+      if(ticketCast && noTickect ) createSeats();
+
+})
+for(let i = 0;i<movies.length; i++) {
+  movies[i].status = false;
+  delete movies[i].userId;
+}
+
+
+let selectedseat = [];
+function createSeats () {
+  document.getElementById("first").innerHTML = ""
+  var boxs = document.createElement("div");
+  boxs.classList.add("boxs");
+  let seats = (localStorage.getItem("seats")) ? JSON.parse(localStorage.getItem("seats")) :movies;
+for(let i = 0 ; i < seats.length; i++) {
+  if(i == 0) {
+    let header = document.createElement("h5");
+    header.classList.add('number');
+    header.innerText = "PREMIUM-Rs.400.00";
+    premium = false;
+    document.getElementById("first").appendChild(header);
+  }
+  
+  if((i - 1) % 10 === 9) {
+    document.getElementById("first").appendChild(boxs);
+    boxs = document.createElement("div");
+    boxs.classList.add("boxs");
+    let alpha = document.createElement('h5');
+    alpha.classList.add("number")
+    alpha.innerText = seats[i].seatId.slice(seats[i].seatId.length);
+    boxs.appendChild(alpha);
+  }
+  if(i == 40) {
+    let header = document.createElement("h5");
+    header.classList.add('number');
+    header.innerText = "STANDARD-Rs.200.00";
+    premium = false;
+    document.getElementById("first").appendChild(header);
+  }
+  let seat = document.createElement("button");
+  seat.classList.add('seat');
+  if(seats[i].status == false) {
+    seat.classList.add('seat');
+    seat.addEventListener("click",() => {
+      if(noTickect !==selectedseat.length) {
+        seat.classList.toggle('Available');
+      seat.classList.toggle('seat-selected');
+      if(seat.classList.contains("seat-selected")) {
+        selectedseat.push(i);
+      } else {
+        let s = (selectedseat.indexOf(i) >= 0) ? selectedseat.indexOf(i) : selectedseat.length;
+        selectedseat.splice(s,1);
+      }
+      }
+    });
+  } else {
+    seat.classList.add("seat-occupied");
+  }
+  if(ticketCast != seats[i].TypeofSeat) {
+    seat.setAttribute("class","seat seat-occupied");
+  }
+  seat.innerText = seats[i].id;
+  boxs.appendChild(seat);
+  }
+}
+function orderSeat(index) {
+  let seats = (localStorage.getItem("seats")) ? JSON.parse(localStorage.getItem("seats")) :movies;
+  seats[index].status = true;
+  localStorage.setItem("seats",JSON.stringify(seats));
+  
+}
+document.getElementById('confirmBook').addEventListener("click" , () => {
+  selectedseat.forEach(id => {
+    orderSeat(id);
+  })
+  createSeats();
+})
